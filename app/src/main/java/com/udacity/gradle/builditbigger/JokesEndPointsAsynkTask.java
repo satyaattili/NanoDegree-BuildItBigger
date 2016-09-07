6048cf82b6b5a2c43612cf2111e9975954549eaf
satyanarayana.avv <satya.attili@gmail.com>
2016-09-07 11:25:20 +0530

    Review comments addressed

--- app/src/main/java/com/udacity/gradle/builditbigger/JokesEndPointsAsynkTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokesEndPointsAsynkTask.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
@@ -43,28 +42,21 @@ public class JokesEndPointsAsynkTask extends AsyncTask<Context, Void, String> {
 
     @Override
     protected String doInBackground(Context... params) {
-        if(myApiService == null) {  // Only do this once
+        if (myApiService == null) {  // Only do this once
 
             MyApi.Builder builder = new
                     MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
                     .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override                         public void initialize(AbstractGoogleClientRequest<?>
-                                                                                         abstractGoogleClientRequest) throws IOException {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?>
+                                                       abstractGoogleClientRequest) throws IOException {
                             abstractGoogleClientRequest.setDisableGZipContent(true);
                         }
                     });
             myApiService = builder.build();
-
-          /*  MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new
-                    AndroidJsonFactory(), null)
-                    .setRootUrl("https://android­app­backend.appspot.com/_ah/api/");
-            myApiService = builder.build();*/
         }
-
-        //mContext = params[0];
-
 
         try {
 
@@ -78,18 +70,9 @@ public class JokesEndPointsAsynkTask extends AsyncTask<Context, Void, String> {
 
     @Override
     protected void onPostExecute(String result) {
-        if(mProgressBar != null){
+        if (mProgressBar != null) {
             mProgressBar.setVisibility(View.GONE);
         }
-
-        /*// Create Intent to launch JokeFactory Activity
-        Intent intent = new Intent(context, DisplayJokeActivity.class);
-        // Put the string in the envelope
-        intent.putExtra(DisplayJokeActivity.JOKE_KEY,result);
-        context.startActivity(intent);
-*/
-       // progressDialog.dismiss();
-        Toast.makeText(mContext, result, Toast.LENGTH_LONG).show();
         mJokeListener.onJokeRecieved(result);
     }
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,9 +2,6 @@ package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 
 import com.udacity.gradle.builditbigger.premium.MainFragment;
 
@@ -18,38 +15,4 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().
                 add(R.id.fragment, new MainFragment()).commit();
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void tellJoke(View view) {
-
-        /*JokesProvider provider = JokesProvider.getJokesProvider();
-        Intent intent = new Intent(this, JokesTellingLibActivity.class);
-        intent.putExtra(getString(R.string.joke_key), provider.getJoke().getJoke());
-        startActivity(intent);*/
-      //  new JokesEndPointsAsynkTask(this, pr).execute(MainActivity.this);
-    }
-
-
 }
